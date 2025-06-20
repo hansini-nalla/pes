29861bde3a601b8ba180b2ae1fe8ed0a316f93b5
PodaMounitha <176000994+PodaMounitha@users.noreply.github.com>
2025-06-20 19:22:50 +0530

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,16 @@
+
 import express from "express";
 import connectDB from "./config/db.ts";
 import studentRoutes from "./routes/student/student.routes.ts";
 import taRoutes from "./routes/ta/ta.routes.ts"; 
-import teacherRoutes from "./routes/teacher/teacher.routes.ts";
 import authRoutes from './routes/authorization/auth.routes.ts';
 import adminroutes from './routes/admin/admin.routes.ts';
 import adminstudentroutes from './routes/admin/student_admin.routes.ts';
 import adminteachroutes from './routes/admin/teacher.routes.ts';
 import dashboardRoutes from './routes/admin/dashboard.ts';
-import admincourseroutes from './routes/admin/course_admin.routes.ts';
+import admincourseroutes from './routes/admin/admin.routes.ts';
+
+
 
 import "./models/Course.ts";
 import "./models/Batch.ts";
@@ -52,8 +54,7 @@ app.use("/api/admin/student",adminstudentroutes);
 app.use("/api/admin/teachers",adminteachroutes);
 app.use("/api/student", studentRoutes);
 app.use('/api/auth', authRoutes);
-app.use('/api/ta', taRoutes); 
-app.use('/api/teacher', teacherRoutes);
+app.use('/api/ta', taRoutes);
 app.use('/api/dashboard', dashboardRoutes);
 app.use("/api/admin/courses", admincourseroutes);
 
