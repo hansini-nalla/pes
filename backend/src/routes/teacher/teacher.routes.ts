47b77c55bcaea9dce24df2cc00fd6b3074a30958
Santpal <santpalkalra1@gmail.com>
2025-06-27 18:03:05 +0530

    Added get exam by course and batch functionality for teacher dashboard

--- backend/src/routes/teacher/teacher.routes.ts
+++ backend/src/routes/teacher/teacher.routes.ts
@@ -3,13 +3,11 @@ console.log("✅ teacher.routes.ts loaded");
 import { Router } from "express";
 import { getTeacherCourses } from "../../controllers/teacher/getTeacherCourses.controller.ts";
 import { authMiddleware } from "../../middlewares/authMiddleware.ts";
+import { getExamsByCourse } from "../../controllers/teacher/getExamsByCourse.controller.ts";
 
 const router = Router();
 router.get("/courses", authMiddleware, getTeacherCourses);
-router.get("/test", (req, res) => {
-  console.log("/api/teacher/test HIT");
-  res.send("Hello from teacher route");
-});
+router.get("/courses/:courseId/exams", authMiddleware, getExamsByCourse);
 
 
 
